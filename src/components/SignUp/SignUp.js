3120a9a8c69b29250a3ba72d59c5937ed24f25e3
Mejbah <mejba.s@gmail.com>
2022-04-10 19:55:53 +0600

    Sign Up button added

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>This Sign Up!!!</h2>
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Signup</h2>
+                <form>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input type="password" name="confirm-password" id="" />
+                    </div>
+                    <input className='form-submit' type="submit" value="Sign Up" />
+                </form>
+                <p className='create-new-account'>
+                    Already have an account?
+                    <Link className='form-link' to="/login"> Login </Link>
+                </p>
+                <div className='or-block'>
+                    <div>----------</div>
+                    <p> or </p>
+                    <div>-----------</div>
+                </div>
+                <button className='btn'>Continue with Google</button>
+            </div>
         </div>
     );
 };

fb0d24d6d6e7306183874d9177dbf302e602dcb0
Mejbah <mejba.s@gmail.com>
2022-04-23 23:18:11 +0600

    about page text littlebit changes...

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>We are secret.</h1>
+            <h1>This is About page!!</h1>
             <p>But you can know about us.</p>
         </div>
     );
